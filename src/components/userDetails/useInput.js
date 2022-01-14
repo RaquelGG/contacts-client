deb67163509bd5697be6f402ed33e4425922478a
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-14 14:05:52 +0100

    hook helper for the form

---
+++ src/components/userDetails/useInput.js
@@ -0,0 +1,22 @@
+import { getByPlaceholderText } from "@testing-library/react";
+import { useState } from "react";
+
+/**
+ * 
+ * @param {*} initialValue 
+ * @returns 
+ */
+export const useInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+
+    return {
+        value,
+        setValue,
+        bind: {
+            value,
+            onChange: event => {
+                setValue(event.target.value);
+            },
+        }
+    };
+};
