30dd81863db288a41e92115edadb143fd54f1504
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-16 15:52:31 +0100

    functional connections

--- src/api-client/conection.js
+++ src/api-client/conection.js
@@ -1,44 +1,80 @@
-import jsonUserList from './userList.json'
-import changeLog from './changeLog.json'
+const URL = "http://127.0.0.1:3000"
 
-//TODO: Send request to server
-export async function getUserList() {
+function prepareJson(user) {
+    return JSON.stringify({
+        name: user.name, 
+        surname: user.surname, 
+        email: user.email,
+        tel: user.tel
+    });
+}
+
+export async function getUsers() {
     try {
-        //const response = await fetch('');
-        //const userList = await response.json();
-        const data = jsonUserList.userlist;
-        return data ? data: null;
+        const response = await fetch(`${URL}/contacts`, {
+            "method": 'GET'
+        });
+        const users = await response.json();
+        return users;
+
     } catch (err) {
-        console.error("Error obtaining the user list: ", err);
+        console.error(`Error getting user list:`, err);
         return null;
     }
-    
 }
 
-export async function getUserChangeLog(id) {
-
+export async function editUser(user) {
+    try {
+        await fetch(`${URL}/contacts/${user.id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: prepareJson(user)
+        });
+    } catch (err) {
+        console.error(`Error editing user ${user.id}:`, err);
+        return null;
+    }
 }
 
-export async function editUser(name, surname, email, tel) {
-
-}
-
-export async function addUser(name, surname, email, tel) {
-
+export async function addUser(user) {
+    try {
+        await fetch(`${URL}/contacts`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: prepareJson(user)
+        });
+    } catch (err) {
+        console.error(`Error adding user ${user.email}:`, err);
+        return null;
+    }
 }
 
 export async function deleteUser(id) {
-
+    try {
+        await fetch(`${URL}/contacts/${id}`, {
+            method: 'DELETE'
+        });
+    } catch (err) {
+        console.error(`Error deleting user ${id}:`, err);
+        return null;
+    }
 }
 
 export async function getChangeLog(id) {
     try {
-        //const response = await fetch('');
-        //const userList = await response.json();
-        const data = changeLog.changeLog;
-        return data ? data: null;
+        const response = await fetch(`${URL}/contacts/${id}/changes`, {
+            method: 'GET',
+        });
+        const changeLog = await response.json();
+        return changeLog;
     } catch (err) {
-        console.error("Error obtaining the user change log: ", err);
+        console.error(`Error getting change log from user ${id}:`, err);
         return null;
     }
 }
