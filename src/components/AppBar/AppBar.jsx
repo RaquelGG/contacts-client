a29d55d8130257530f1c6b7556e67b45bffdb313
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-19 00:23:27 +0100

    now appbar can habe different buttons

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import IconButton from './IconButton'
 
 import arrowBackIcon from '../../img/arrow_back_24dp.svg';
 
-function AppBar({ children, title, canGoBack}) {
+function AppBar({ children, title, goBack }) {
     return <Wrapper>
         {<Nav>
             <Left>
-                {canGoBack 
-                    ? <IconButton src={arrowBackIcon} to='/' />
+                {goBack
+                    ? <StyledLink to={goBack}>
+                        <IconButton src={arrowBackIcon}  />
+                    </StyledLink>
                     : null
                 }
                 <Title>{title}</Title>
@@ -25,22 +29,26 @@ function AppBar({ children, title, canGoBack}) {
 
 /* STYLED COMPONENTS */
 const Wrapper = styled.div`
-    position: fixed;
+    position: sticky;
     background-color: #1b1b1b;
     top: 0;
     height: 60px;
     width: 100%;
     box-shadow: 0px 4px 2px 0px #1441a1;
+    display: flex;
+    justify-content: center;
 `;
 
 const Nav = styled.nav`
     display: flex;
     width: 100%;
-    padding: 0 16px;
+    padding: 0;
     flex-wrap: nowrap;
     justify-content: space-between;
     align-items: center;
     height: 100%;
+    max-width: 800px;
+    //min-width: 500px;
 `;
 
 const Left = styled.div`
@@ -55,6 +63,11 @@ const Right = styled(Left)``;
 const Title = styled.h1`
     color: white;
     font-weight: 400;
+    font-size: 1.1rem;
+`;
+
+const StyledLink = styled(Link)`
+    margin-bottom: -6px;
 `;
 
 export default AppBar;
