d7a909ad64ab991b3a238a813cd82e31e758cabd
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-19 00:19:44 +0100

    extract code to constants for better understanding

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -2,58 +2,66 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
+import Wrapper from '../Wrapper'
 import PopError from '../PopError'
 import ContactIcon from './ContactIcon'
 import AddContactButton from './AddContactButton'
 import sadIcon from '../../img/sad.png'
-import AppBar from '../AppBar/AppBar'
+import AppBar from '../appBar/AppBar'
 
 function ContactList({ contacts }) {
-  return (
-    <Wrapper>
-      <AppBar title="Contact list" canGoBack={false} />
-      {contacts ? (
-        <List>
-          {Object.entries(contacts).map(([id, contact]) =>
-            <ListItem
-              key={id}
-              to={`/contact/${id}`}
-            >
-              <ContactIcon contact={contact} />
-              <Name>{contact.name} {contact.surname}</Name>
-            </ListItem>
-          )}
-        </List>
-      ) : (
-        <>
-          
-          <VerticalAlignCenter>
-            <Img src={sadIcon} alt="Depression icons created by max.icons - Flaticon" longdesc="https://www.flaticon.com/free-icons/depression" />
-          </VerticalAlignCenter>
-          <PopError errorMessage="Failed to load. No response from the server" />
-        </>
+
+  const emptyContacts = (
+    <VerticalAlignCenter>
+      <Img src={sadIcon} alt="Depression icons created by max.icons - Flaticon" longdesc="https://www.flaticon.com/free-icons/depression" />
+    </VerticalAlignCenter>);
+
+  const generateContactList = (
+    <List>
+      {Object.entries(contacts).map(([id, contact]) =>
+        <ListItem
+          key={id}
+          to={`/contact/${id}`}
+        >
+          <ContactIcon contact={contact} />
+          <Name>{contact.name} {contact.surname}</Name>
+        </ListItem>
       )}
-      <AddContactButton />
-    </Wrapper>
+    </List>
   );
+
+  return <>
+      <AppBar title="Contact list" />
+      <Wrapper>
+        {Object.keys(contacts).length === 0
+          ? emptyContacts
+          : contacts
+            ? generateContactList
+            : <PopError errorMessage="Failed to load. No response from the server" />
+        }
+        <AddContactButton />
+      </Wrapper>
+    </>;
 }
 
 /* STYLED COMPONENTS */
-const Wrapper = styled.div``
-
 const List = styled.ol`
   padding: 0;
+  width: 100%;
 `;
 
 const ListItem = styled(Link)`
   display: flex;
   padding: 16px 40px;
   text-decoration: none;
-  color: black;
+  color: #000000;
+  width: 100%;
+  border: 2px solid white;
+  border-radius: 10px;
 
   &:hover {
     cursor: pointer;
-    background-color: #00000014;
+    border: 2px solid #1441a1;
   }
 `;
 
@@ -67,15 +75,15 @@ const VerticalAlignCenter = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
-  margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
-`
+  margin-top: -92px;
+`;
 
 const Img = styled.img`
   margin: auto;
   width: 200px;
-`
+`;
 
 export default ContactList;
