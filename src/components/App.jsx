447055a2a03803aad791b90f7ed609672982b7a5
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-20 15:57:31 +0100

    show error if there is error or no response from server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,11 +11,12 @@ import { getContacts } from '../api-client/api-client';
 import Spinner from './common/Spinner';
 import EditContact from './contactManagement/EditContact';
 import AddContact from './contactManagement/AddContact';
+import PopError from './common/PopError';
 
 function App() {
     const [contacts, setContacts] = useState(null);
     const [processing, setProcessing] = useState(true);
-   
+
     async function fetchContacts() {
         setContacts(await getContacts());
         setProcessing(false);
@@ -25,23 +26,26 @@ function App() {
         fetchContacts();
     }, []);
 
-
     return <BrowserRouter>
-        {processing ?
-            <Routes>
+        {processing
+            ? <Routes>
                 <Route path='contact/changelog/:contactId' element={<ChangeLog />} />
                 <Route path='*' element={<Spinner />} />
-            </Routes> : <Routes>
-                <Route path='/' element={<ContactList contacts={contacts} />} />
-                <Route path='contact/:contactId' element={
-                    <EditContact contacts={contacts} fetchContacts={fetchContacts} />
-                } />
-                <Route path='contact/new' element={
-                    <AddContact contacts={contacts} fetchContacts={fetchContacts} />
-                } />
-                <Route path='contact/changelog/:contactId' element={<ChangeLog />} />
-                <Route path='*' element={<ContactList contacts={contacts} />} />
-            </Routes>}
+            </Routes>
+            : contacts
+                ? <Routes>
+                    <Route path='/' element={<ContactList contacts={contacts} />} />
+                    <Route path='contact/:contactId' element={
+                        <EditContact contacts={contacts} fetchContacts={fetchContacts} />
+                    } />
+                    <Route path='contact/new' element={
+                        <AddContact contacts={contacts} fetchContacts={fetchContacts} />
+                    } />
+                    <Route path='contact/changelog/:contactId' element={<ChangeLog />} />
+                    <Route path='*' element={<ContactList contacts={contacts} />} />
+                </Routes>
+                : <PopError errorMessage='Failed to load. No response from the server' />
+        }
     </BrowserRouter>;
 }
 
